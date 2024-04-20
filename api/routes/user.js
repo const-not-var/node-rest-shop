1936ac233a0849430b14e1e750e9daf87550b3b4
const-not-var <161810593+const-not-var@users.noreply.github.com>
2024-04-20 13:11:40 -0500

    JWT and bug fix in signup

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -14,7 +15,7 @@ router.post("/signup", (req, res, next) => {
                     message: 'Email already exists'
                   });
               } else {
-                bcrypt.hash(req.body.email, 10, (err, hash) => {
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
                     if(err) {
                         return res.status(500).json({
                             error: err
@@ -54,18 +55,28 @@ router.post("/login", (req, res, next) => {
                 });
             }
             bcrypt.compare(req.body.password, user[0].password, (err, result) => {
-            if (err) {
-                return res.status(401).json({
+                if (err) {
+                    return res.status(401).json({
+                        message: 'Auth failed'
+                    });
+                }
+                if (result) {
+                    const token = jwt.sign({
+                        email: user[0].email,
+                        userId: user[0]._id
+                    }, 
+                    process.env.JWT_KEY, 
+                    {
+                        expiresIn: "1h"
+                    }
+                );
+                    return res.status(200).json({
+                        message: 'Auth successful',
+                        token: token
+                    });
+                }
+                res.status(401).json({
                     message: 'Auth failed'
-                });
-            }
-            if (result) {
-                return res.status(200).json({
-                    message: 'Auth successful'
-                });
-            }
-            res.status(401).json({
-                message: 'Auth failed'
                 });
             });
         })
